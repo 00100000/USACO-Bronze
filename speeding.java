98c8591b4b41a78b190d45291be1affd40f0e8e6
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2021-01-10 00:20:41 -0500

    Initial commit

---
+++ speeding.java
@@ -0,0 +1,54 @@
+/*
+ID: 00100000
+LANG: JAVA
+TASK: speeding
+*/
+import java.util.*;
+import java.io.*;
+
+class speeding {
+    public static void main (String[]args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("speeding.in"));
+        StringTokenizer t = new StringTokenizer(r.readLine());
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("speeding.out")));
+
+        int worstInfraction = 0;
+
+        int n = Integer.parseInt(t.nextToken());
+        int m = Integer.parseInt(t.nextToken());
+
+        int[] nArr = new int[100];
+        int[] mArr = new int[100];
+        // add all values of n by each mile
+        int truePosN = 0;
+        for (int i = 0; i < n; i++) {
+            t = new StringTokenizer(r.readLine());
+            int segmentLength = Integer.parseInt(t.nextToken());
+            int segmentSpeed = Integer.parseInt(t.nextToken());
+            for (int j = 0; j < segmentLength; j++) {
+                nArr[truePosN] = segmentSpeed;
+                truePosN++;
+            }
+        }
+        // add all values of m by each mile
+        int truePosM = 0;
+        for (int i = 0; i < m; i++) {
+            t = new StringTokenizer(r.readLine());
+            int segmentLength = Integer.parseInt(t.nextToken());
+            int segmentSpeed = Integer.parseInt(t.nextToken());
+            for (int j = 0; j < segmentLength; j++) {
+                mArr[truePosM] = segmentSpeed;
+                truePosM++;
+            }
+        }
+        // check all values in each array to find worst infraction
+        for (int i = 0; i < 100; i++) {
+            if (mArr[i] - nArr[i] > worstInfraction) {
+                worstInfraction = mArr[i] - nArr[i];
+            }
+        }
+        pw.println(worstInfraction);
+        pw.close();
+        r.close();
+    }
+}
