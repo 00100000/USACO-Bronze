6fa783ab5fdc1b6d7311da7a16634b4e0fdad77a
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2021-03-02 20:30:43 -0500

    Uddered But Not Herd

---
+++ problems/prob1_bronze_jan21.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class UdderedButNotHerd {
+    public static void main (String[]args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String cowphabet = r.readLine();
+        String heard = r.readLine();
+        // default is 1 because my algorithm doesn't count the first letter
+        int timesHummed = 1;
+
+        int[] positions = new int[heard.length()];
+        // put all positions of letters into positions[]
+        for (int i = 0; i < heard.length(); i++) {
+            for (int j = 0; j < cowphabet.length(); j++) {
+                if (heard.charAt(i) == cowphabet.charAt(j)) {
+                    positions[i] = j;
+                    break;
+                }
+            }
+        }
+        // check for all values of position where the next letter is
+        // in a lower position than the current letter
+        for (int i = 0; i < positions.length - 1; i++) {
+            if (positions[i] >= positions[i + 1]) {
+                timesHummed++;
+            }
+        }
+
+        System.out.println(timesHummed);
+        r.close();
+    }        
+}
