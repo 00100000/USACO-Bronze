86fc16d19f3b4e0661e067cd2f0abe53b3ec0f76
00100000spacebar <00100000spacebar@gmail.com>
2021-06-29 21:58:44 -0400

    Shell Game

---
+++ bronze/shell.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+class shell {
+	public static void main(String[]args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("shell.in"));
+		PrintWriter pw = new PrintWriter(new FileWriter("shell.out"));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] pebbles = {0, 1, 2};
+		int[] picked = {0, 0, 0};
+		// simulate
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1, b = Integer.parseInt(st.nextToken()) - 1, g = Integer.parseInt(st.nextToken()) - 1;
+			int temp = pebbles[a];
+			pebbles[a] = pebbles[b];
+			pebbles[b] = temp;
+
+			picked[pebbles[g]]++;
+		}
+
+		int max = 0;
+		for (int i = 0; i < 3; i++) {
+			if (picked[i] > max) max = picked[i];
+		}
+		pw.println(max);
+		br.close();
+		pw.close();
+	}
+}
