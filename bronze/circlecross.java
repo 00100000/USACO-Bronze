e24f46a65a820be3e8e9e3f41ad83b293cf16668
00100000spacebar <00100000spacebar@gmail.com>
2021-07-09 19:48:26 -0400

    Better Solution

--- bronze/circlecross.java
+++ bronze/circlecross.java
@@ -1,49 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
 
 class circlecross {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("circlecross.in"));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("circlecross.out")));
+		PrintWriter pw = new PrintWriter(new FileWriter("circlecross.out"));
 
-		char[] circle = st.nextToken().toCharArray();
-		int crossingPaths = 0;
+		char[] circle = br.readLine().toCharArray();
 
+		int crosses = 0;
 		for (int i = 0; i < 26; i++) {
-			// find positions of letters i and j
-			int[] pos = { -1, 0 };
+			int[] pos = {-1, 0};
 			for (int j = 0; j < 52; j++) {
-				if (65 + i == circle[j]) {
+				if (circle[j] == i + 65) {
 					if (pos[0] == -1) pos[0] = j; else pos[1] = j;
 				}
 			}
-			// letters in between i and j
-			char[] between = new char[pos[1] - pos[0]];
-			for (int j = pos[0], bj = 0; j < pos[1]; j++, bj++) {
-				between[bj] = circle[j];
-			}
-			// check if each letter is alone
-			for (int j = 0; j < between.length; j++) {
-				// don't check for letters below i alphabetically
-				// or there will be 2 crossing paths for 1
-				if (65 + i < (int) between[j]) {
+
+			for (int j = pos[0]; j < pos[1]; j++) {
+				if (i + 65 < circle[j]) {
 					int seen = 0;
-					for (int k = 0; k < between.length; k++) {
-						if (between[j] == between[k]) seen++;
+					for (int k = pos[0]; k < pos[1]; k++) {
+						if (circle[j] == circle[k]) seen++;
 					}
-					if (seen == 1) crossingPaths++;
+					if (seen == 1) crosses++;
 				}
 			}
-
 		}
 
-		pw.println(crossingPaths);
+		pw.println(crosses);
 		br.close();
 		pw.close();
 	}
