7da465717c3041c40ec157b6b8c68d14858f35ab
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2021-07-19 00:58:52 -0400

    Better Solution

--- bronze/prob3_bronze_jan21.java
+++ bronze/prob3_bronze_jan21.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -10,40 +8,30 @@ class prob3_bronze_jan21 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-
-		long ret = 1;
 
 		int n = Integer.parseInt(st.nextToken());
 
-		// construct cow and stall array
 		int[] cows = new int[n];
 		int[] stalls = new int[n];
-		StringTokenizer cowsString = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			cows[i] = Integer.parseInt(cowsString.nextToken());
-		}
-		StringTokenizer barnHeightsString = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			stalls[i] = Integer.parseInt(barnHeightsString.nextToken());
-		}
-
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) cows[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) stalls[i] = Integer.parseInt(st.nextToken());
+		// solve
 		Arrays.sort(cows);
 		Arrays.sort(stalls);
 
-		int temp = 0;
+		long arrangements = 1;
+		// find lowest stall j each cow i can fit into
 		for (int i = n - 1; i > -1; i--) {
+			int lowest = 0;
 			for (int j = i; j > -1; j--) {
-				// find lowest stall
-				if (cows[i] <= stalls[j]) temp++;
+				if (cows[i] <= stalls[j]) lowest++;
 			}
-			ret *= temp;
-			// reset
-			temp = 0;
+			arrangements *= lowest;
 		}
 
-		pw.println(ret);
+		System.out.println(arrangements);
 		br.close();
-		pw.close();
 	}
 }
