d29d11171ca8a1097a97ae0c28fd849fd35e25ab
00100000spacebar <00100000spacebar@gmail.com>
2021-08-05 23:25:15 -0400

    The Bovine Shuffle

---
+++ bronze/shuffle.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+class shuffle {
+	public static void main(String[]args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("shuffle.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("shuffle.out")));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] pos = new int[n];
+		int[] cows = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			// swap positions to make cows go the opposite direction of a shuffle
+			pos[Integer.parseInt(st.nextToken()) - 1] = i;
+			cows[i] = Integer.parseInt(st2.nextToken());
+		}
+		// simulate backwards shuffle
+		for (int i = 0; i < 3; i++) {
+			int[] temp = new int[n];
+			for (int j = 0; j < n; j++) {
+				temp[pos[j]] = cows[j];
+			}
+			cows = temp;
+		}
+		for (int i = 0; i < n; i++) {
+			pw.println(cows[i]);
+		}
+
+		br.close();
+		pw.close();
+	}
+}
