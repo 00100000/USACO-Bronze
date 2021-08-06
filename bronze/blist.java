c41d7351f1bf1ac843a392cfec2d4d20620b6c7d
00100000spacebar <00100000spacebar@gmail.com>
2021-08-05 23:53:24 -0400

    The Bucket List

---
+++ bronze/blist.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+class blist {
+	public static void main(String[]args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("blist.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("blist.out")));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] s = new int[n];
+		int[] t = new int[n];
+		int[] b = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			s[i] = Integer.parseInt(st.nextToken()) - 1;
+			t[i] = Integer.parseInt(st.nextToken()) - 1;
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+		// simulate each time t
+		int max = 0;
+		for (int i = 0; i < 1000; i++) {
+			int needed = 0;
+			for (int j = 0; j < n; j++) {
+				if (i > s[j] && i < t[j]) {
+					needed += b[j];
+				}
+			}
+			max = Math.max(max, needed);
+		}
+
+		pw.print(max);
+		br.close();
+		pw.close();
+	}
+}
