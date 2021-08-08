0d750280223761d6d29705d78cd97a34f24041cc
00100000spacebar <00100000spacebar@gmail.com>
2021-08-07 23:26:32 -0400

    Censor - More Efficient Solution timed out cases

--- bronze/censor.java
+++ bronze/censor.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 class censor {
 	public static void main(String[]args) throws IOException {
@@ -12,10 +13,32 @@ class censor {
 
 		String s = br.readLine();
 		String t = br.readLine();
-		// literally LCP lol
-		while (s.contains(t)) s = s.replaceAll(t, "");
 
-		pw.print(s);
+		char[] res = new char[s.length()];
+		Arrays.fill(res, ' ');
+		for (int i = 0, resI = 0; i < s.length(); i++, resI++) {
+			res[resI] = s.charAt(i);
+			// check last characters of res to see if they are equal to t
+			int similar = 0;
+			if (resI >= t.length()) for (int j = resI - t.length() + 1, k = 0; j <= resI; j++, k++) {
+				if (res[j] == t.charAt(k)) {
+					similar++;
+				} else {
+					break;
+				}
+			}
+			// if they are, remove the end of t characters of res
+			if (similar == t.length()) {
+				for (int j = resI - t.length() + 1; j <= resI; j++) {
+					res[j] = ' ';
+				}
+				resI -= t.length();
+			}
+		}
+		for (int i = 0; i < s.length(); i++) {
+			if (res[i] == ' ') break;
+			pw.print(res[i]);
+		}
 		br.close();
 		pw.close();
 	}
