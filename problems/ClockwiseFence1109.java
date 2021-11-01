a6f178e07a4ec90bc4993f7ad592f3c69ceb3757
32n <00100000spacebar@gmail.com>
2021-11-01 15:51:51 -0400

    Clockwise Fence

---
+++ problems/ClockwiseFence1109.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+class ClockwiseFence1109 {
+	public static void main(String[]args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+		int n = Integer.parseInt(br.readLine());
+
+		String[] fence = new String[n];
+		for (int i = 0; i < n; i++) {
+			fence[i] = br.readLine();
+		}
+
+		for (int i = 0; i < n; i++) {
+			// check if totalDir > 0 or < 0
+			int totalDir = 0;
+			for (int j = 0; j < fence[i].length() - 1; j++) {
+				totalDir += rOrL(fence[i].charAt(j), fence[i].charAt(j + 1));
+			}
+			pw.println(totalDir > 0 ? "CW" : "CCW");
+		}
+
+		br.close();
+		pw.close();
+	}
+	// returns 1 if b is to the right of a, -1 if b is to the left of a, and 1 otherwise
+	static int rOrL(char a, char b) {
+		switch (a) {
+			case 'N':
+				switch (b) {
+					case 'E':
+						return 1;
+					case 'W':
+						return -1;
+				}
+				break;
+			case 'S':
+				switch (b) {
+					case 'E':
+						return -1;
+					case 'W':
+						return 1;
+				}
+				break;
+			case 'E':
+				switch (b) {
+					case 'N':
+						return -1;
+					case 'S':
+						return 1;
+				}
+				break;
+			case 'W':
+				switch (b) {
+					case 'N':
+						return 1;
+					case 'S':
+						return -1;
+				}
+				break;
+		}
+		return 0;
+	}
+}
